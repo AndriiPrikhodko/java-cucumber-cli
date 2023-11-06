747cc637a5502f02b753f762cb91c25fd01c4fcf
Andrii Prykhodko <andriiprykhodko@Andriis-Air.home>
2023-11-06 16:22:47 +0000

    Task4 cucumber-junit

---
+++ src/test/java/cucumber/stepdefinitions/VowelsUnitStepDefs.java
@@ -0,0 +1,47 @@
+package cucumber.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import org.junit.Assert;
+
+import com.exercise.VowelCounter;
+import com.exercise.CountResult;
+
+import io.cucumber.java.en.Then;
+
+public class VowelsUnitStepDefs {
+    private VowelCounter vowelCounter;
+    private List<CountResult> result;
+
+    @Given("I have a VowelCounter$")
+    public void i_have_a_VowelCounter() {
+        this.vowelCounter = new VowelCounter();
+    }
+
+    @When("I passed arguments to getVowelListWithCount {string}")
+    public void i_passed_arguments_to_getVowelListWithCount(String args) {
+        String[] arrArgs = args.split(" ");
+        this.result = this.vowelCounter.getVowelListWithCount(arrArgs);
+    }
+
+    @Then("the output result should be {string}")
+    public void the_output_result_should_be(String expectedResult) {
+        String actualResult = this.result.toString();
+        assertEquals(actualResult, expectedResult);
+    }
+
+    @Then("the output result is null")
+    public void the_output_result_is_null() {
+        assertEquals(this.result, null);
+    }
+}
